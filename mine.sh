174a7b70c2dedfef68d938028418e5f4c3282e1c
RiSET <33034757+RiSET-NET@users.noreply.github.com>
2025-03-20 13:10:16 +0000

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#Solo -u RLiHUxS777oDkRDi8o1Sh6DRGw2ZqEX8FE.Linux -p x --cpu "$(nproc)"
